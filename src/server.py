ec574f13d0508b5bc159ccb6841abbcd0b04b4bd
Njoguu <njoguuallan@gmail.com>
2022-11-01 09:23:01 +0300

    API Blueprint

--- src/server.py
+++ src/server.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/api/v1')
-def hello_world():
-    return 'Hello World'
+@app.route('/api/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/api/v1/')
+def versions():
+    return render_template('versions.html')
+
+@app.route('/api/v1/jobs')
+def available_jobs():
+    return render_template('apiview.html')
+    
