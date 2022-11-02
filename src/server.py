9df2c1636c67445fd66fa6b3910db8f68a054a7e
Njoguu <njoguuallan@gmail.com>
2022-11-02 19:59:38 +0300

    Adding Front-end

--- src/server.py
+++ src/server.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+# from flask_rest import Api, Resource
+import requests
+from bs4 import BeautifulSoup
+import json
+# import time
 
-@app.route('/api/')
+app = Flask(__name__)
+# api = Api(app)
+
+@app.route('/')
 def homepage():
     return render_template('index.html')
 
@@ -10,6 +17,58 @@ def versions():
     return render_template('versions.html')
 
 @app.route('/api/v1/jobs')
+# class AvailableJobs(Resource):
+    # def get(self):
+    #     return {'hello': 'world'}
 def available_jobs():
-    return render_template('apiview.html')
+
+    # return jsonify(spec.to_dict())
+    # for page in range(1,4):
+    res = requests.get('https://www.kenyamoja.com/jobs?page=')
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    # Content
+    raw_jobs = soup.select('.items-title')
+    raw_links = soup.find_all('div', 'news-title')
+    raw_dates_posted = soup.select('.items-date')
+
+    # return clean content
+    def clean_content(raw_links):
+        # return clean links
+        links = []
+        for link in raw_links:
+            lnk = link.a.get('href').replace('&utm_source=KenyaMOJA.com', '')
+            clean_links = lnk[lnk.find('https'):]
+            links.append(clean_links)
+        
+        return links
+
+    def sort_jobs(info):
+        return sorted(info, key= lambda k:k['Title'])
+
+
+    def create_info(raw_jobs, links, raw_dates_posted):
+        info = []
+        for index, job in enumerate(raw_jobs):
+            job_title = raw_jobs[index].getText()
+            job_link = links[index]
+            date_posted = raw_dates_posted[index].getText().strip()
+            info.append(
+                {
+                'Title' : job_title,
+                'Application Link' : job_link,
+                'Date Posted' : date_posted
+                }
+                )
+        all_jobs = sort_jobs(info)
+        json_object = json.dumps(all_jobs, indent=4)
+        return json_object
+
+    return create_info(raw_jobs,clean_content(raw_links),raw_dates_posted)
+
+
+    
+# @app.route('/api/v1/jobs/<int:job_id>')
+# def specific_available_jobs(job_id):
+#     return render_template('specific_job', job_id=job_id)
     
