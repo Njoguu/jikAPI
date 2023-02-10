9d3aa4cb23ff416a03b97b46273d038a61e11648
whoisnjoguu <60213982+Njoguu@users.noreply.github.com>
2023-02-11 00:02:21 +0300

    Update database.py

--- src/backend/database.py
+++ src/backend/database.py
@@ -14,7 +14,7 @@ def getConnection():
         database = os.environ['DATABASE_NAME'],
         user = os.environ['USER'],
         password = os.environ['PASSWORD'],
-        port = os.environ['PORT'],
+        port = '5432',
     )
     
     return conn
