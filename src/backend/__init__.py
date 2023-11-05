b2cc1176b9ef102bcf23b880177f22d04c16a304
whoisnjoguu <60213982+Njoguu@users.noreply.github.com>
2023-11-05 16:13:11 +0000

    modified redis url

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -59,7 +59,7 @@ def create_app(test_config=None):
                 key_func=get_remote_address,
                 enabled=True,
                 default_limits=["40 per day", "5 per minute"],
-                storage_uri=os.environ["REDIS_URL"] + "/1",  
+                storage_uri=os.environ["REDIS_URL"] + "/1" + "?ssl_cert_reqs=none",  
                 storage_options={"socket_connect_timeout": 30},
                 strategy="fixed-window"
             )
